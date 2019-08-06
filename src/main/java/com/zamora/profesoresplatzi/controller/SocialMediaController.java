c638c46a62a74da46bd05bc5a32c8293caa1e254
Andrés Zamora <andreszamora1998@gmail.com>
2019-08-06 16:27:32 -0500

    Definiendo Metodo GET y Metodo POST

--- src/main/java/com/zamora/profesoresplatzi/controller/SocialMediaController.java
+++ src/main/java/com/zamora/profesoresplatzi/controller/SocialMediaController.java
@@ -4,11 +4,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import com.zamora.profesoresplatzi.model.SocialMedia;
 import com.zamora.profesoresplatzi.service.SocialMediaService;
@@ -31,6 +35,23 @@ public class SocialMediaController {
 			return new ResponseEntity<List<SocialMedia>>(socialMedias,HttpStatus.OK);
 		}
 		
+	}
+	
+	@RequestMapping(value= "/socialMedias", method = RequestMethod.POST, headers = "Accept=application/json")
+	public ResponseEntity<?> createSocialMedia(@RequestBody SocialMedia socialMedia, UriComponentsBuilder uriComponentBuilder){
+		if(socialMedia.getName().equals(null) || socialMedia.getName().isEmpty()) {
+			
+			return new ResponseEntity(HttpStatus.NO_CONTENT);
+		}
+		if(_socialMediaService.FindByName(socialMedia.getName())!= null) {
+			return new ResponseEntity(HttpStatus.NO_CONTENT);
+		}
+		_socialMediaService.saveSocialMedia(socialMedia);
+		SocialMedia socialMedia2 = _socialMediaService.FindByName(socialMedia.getName());
+		HttpHeaders headers = new HttpHeaders();
+		headers.setLocation(uriComponentBuilder.path("/v1/socialMedias/{id}").buildAndExpand(socialMedia2.getIdSocialMedia()).toUri());
+		
+		return new ResponseEntity<String>(headers,HttpStatus.CREATED);
 	}
 	
 }
