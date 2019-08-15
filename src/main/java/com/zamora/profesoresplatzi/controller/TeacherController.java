f794c2825b6731fc0540da16c3f1a3e5c9a948c6
Andrés Zamora <andreszamora1998@gmail.com>
2019-08-15 15:56:38 -0500

    Subir Imagenes en el API Rest

--- src/main/java/com/zamora/profesoresplatzi/controller/TeacherController.java
+++ src/main/java/com/zamora/profesoresplatzi/controller/TeacherController.java
@@ -1,11 +1,18 @@
 package com.zamora.profesoresplatzi.controller;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +20,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import com.zamora.profesoresplatzi.model.Teacher;
@@ -116,5 +124,50 @@ public class TeacherController {
 		
 		return new ResponseEntity(HttpStatus.OK);
 	}
-
+	
+	public static final String TEACHER_UPLOADED_FOLDER="images/teachers/";
+	//CREATE TEACHER IMAGE
+	@RequestMapping(value="teachers/image",method=RequestMethod.POST,headers=("content-type=multipart/form-data"))
+	public ResponseEntity<byte[]> uploadTeacherImage(@RequestParam("id_teacher") Long idTeacher, @RequestParam("file") MultipartFile multipartfile, UriComponentsBuilder uriComponentBuilder){
+		if(idTeacher == null) {
+			return new ResponseEntity(new CustomErrorType("Please set id_Teacher"),HttpStatus.CONFLICT);
+		}
+		if(multipartfile.isEmpty()) {
+			return new ResponseEntity(new CustomErrorType("Please seselect a file to upload"),HttpStatus.CONFLICT);
+		}
+		
+		Teacher teacher = _teacherService.findTeacherById(idTeacher);
+		if(teacher==null){
+			return new ResponseEntity(new CustomErrorType("Teacher With id: "+ idTeacher + " Not Found"),HttpStatus.NOT_FOUND);
+		}
+		
+		if(teacher.getAvatar().isEmpty() || teacher.getAvatar() != null) {
+			String filename = teacher.getAvatar();
+			Path path = Paths.get(filename);
+			File f = path.toFile();
+			if(f.exists()) {
+				f.delete();
+			}
+		}
+		try {
+			Date date = new Date();
+			SimpleDateFormat dateformat = new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss");
+			 String dateName = dateformat.format(date);
+			 
+			 String fileName = String.valueOf(idTeacher) + "-pictureTeacher-"+ dateName + "." + multipartfile.getContentType().split("/")[1];
+			 teacher.setAvatar(fileName);
+			 
+			 byte[] bytes = multipartfile.getBytes();
+			 Path path = Paths.get(TEACHER_UPLOADED_FOLDER+ fileName);
+			 Files.write(path, bytes);
+			 
+			 _teacherService.updateTeacher(teacher);
+			 return ResponseEntity.ok().contentType(MediaType.IMAGE_JPEG).body(bytes);
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+			return new ResponseEntity(new CustomErrorType("Error During Upload"),HttpStatus.NOT_FOUND);
+		}
+		
+	} 
 }
